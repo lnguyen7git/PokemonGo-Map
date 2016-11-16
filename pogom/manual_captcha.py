20233f6fcb1b314a46d8faa9fb1fa9375f6e6fc1
ic4roswings <angelsotomayor@msn.com>
2016-11-16 17:32:41 -0600

    Update manual_captcha.py

--- pogom/manual_captcha.py
+++ pogom/manual_captcha.py
@@ -12,26 +12,26 @@ def captcha_verifier(captcha_url):
     driver.get("https://pgorelease.nianticlabs.com/")
     driver.get(captcha_url)
     ex_script = '''window.globalVariable = "Fail";
-var captchaPage = '<form action="?" method="POST"><div class="g-recaptcha" data-size="compact" data-sitekey="6LeeTScTAAAAADqvhqVMhPpr_vB9D364Ia-1dSgK" data-callback="captchaResponse"></form>'; 
-document.body.parentElement.innerHTML = captchaPage; 
-var script = document.createElement('script'); 
-script.src = 'https://www.google.com/recaptcha/api.js?hl=en'; 
-script.type = 'text/javascript'; 
+var captchaPage = '<form action="?" method="POST"><div class="g-recaptcha" data-size="compact" data-sitekey="6LeeTScTAAAAADqvhqVMhPpr_vB9D364Ia-1dSgK" data-callback="captchaResponse"></form>';
+document.body.parentElement.innerHTML = captchaPage;
+var script = document.createElement('script');
+script.src = 'https://www.google.com/recaptcha/api.js?hl=en';
+script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
-var script2 = document.createElement('script'); 
-script2.type = 'text/javascript'; 
+var script2 = document.createElement('script');
+script2.type = 'text/javascript';
 script2.text = 'function captchaResponse(str) {window.globalVariable = str;}'
 document.getElementsByTagName('head')[0].appendChild(script2);
 '''
-    element = driver.execute_script(ex_script)
+    driver.execute_script(ex_script)
     try:
         WebDriverWait(driver, 30).until(EC.text_to_be_present_in_element_value((By.ID, "g-recaptcha-response"), ""))
     except:
         print('Timed Out')
-        
     linea = driver.execute_script("return globalVariable;")
     driver.close()
     return linea
+
 
 def chrome_verifier():
     try:
@@ -39,4 +39,4 @@ def chrome_verifier():
         driver.close()
         return True
     except:
-         return False
+        return False
