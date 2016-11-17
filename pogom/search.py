3394234d80bceb0921aacf889bfcf8281ce4a4a3
ic4roswings <angelsotomayor@msn.com>
2016-11-17 15:22:58 -0600

    Update search.py

--- pogom/search.py
+++ pogom/search.py
@@ -456,7 +456,7 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                                 else:
                                     status['message'] = 'Account {} is encountering a captcha, starting response sequence'.format(account['username'])
                                     log.warning(status['message'])
-                                    captcha_token = captcha_verifier(captcha_url,status)
+                                    captcha_token = captcha_verifier(captcha_url, status)
                                     status['message'] = 'Retrieved captcha token, attempting to verify challenge for {}'.format(account['username'])
                                     log.info(status['message'])
                                     if captcha_token == 'Fail':
@@ -479,21 +479,21 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                                 status['message'] = 'Account {} is encountering a captcha, starting 2captcha sequence'.format(account['username'])
                                 log.warning(status['message'])
                                 captcha_token = token_request(args, status, captcha_url)
-    
+
                                 if 'ERROR' in captcha_token:
                                     log.warning("Unable to resolve captcha, please check your 2captcha API key and/or wallet balance")
                                     account_failures.append({'account': account, 'last_fail_time': now(), 'reason': 'catpcha failed to verify'})
                                     break
-    
+
                                 else:
                                     status['message'] = 'Retrieved captcha token, attempting to verify challenge for {}'.format(account['username'])
                                     log.info(status['message'])
                                     response = api.verify_challenge(token=captcha_token)
-    
+
                                     if 'success' in response['responses']['VERIFY_CHALLENGE']:
                                         status['message'] = "Account {} successfully uncaptcha'd".format(account['username'])
                                         log.info(status['message'])
-    
+
                                     else:
                                         status['message'] = "Account {} failed verifyChallenge, putting away account for now".format(account['username'])
                                         log.info(status['message'])
